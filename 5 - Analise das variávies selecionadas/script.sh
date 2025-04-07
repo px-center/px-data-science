b6044cdb132238976eaed31f2eeb6979178f4ed2
deboragomespx <debora.gomes@px.center>
2025-04-07 11:37:02 -0300

    kickoff idh

--- 5 - Analise das variávies selecionadas/script.sh
+++ 5 - Analise das variávies selecionadas/script.sh
@@ -1,3 +1,3 @@
-virtualenv idh_px_venv
-source idh_px_venv/bin/activate
-idh_px_venv/bin/pip install -r requirements.txt
+virtualenv .idh_px_venv
+source .idh_px_venv/bin/activate
+.idh_px_venv/bin/pip install -r requirements.txt
